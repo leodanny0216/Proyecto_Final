990efd652b06fbaefca0f1ac736a1d3d7f51b117
leodanny0216 <leodannymaria2@gmail.com>
2025-12-04 01:16:09 -0400

    cree modelos de transferencia de datos, CategoriaDto: id, nombre, descripción

--- app/src/main/java/edu/ucne/proyecto_final/data/remote/dto/CategoriaDto.kt
+++ app/src/main/java/edu/ucne/proyecto_final/data/remote/dto/CategoriaDto.kt
@@ -2,7 +2,6 @@ package edu.ucne.proyecto_final.data.remote.dto
 
 import kotlinx.serialization.Serializable
 import java.util.Date
-
 @Serializable
 data class CategoriaDto(
     val categoriaId: Int = 0,
