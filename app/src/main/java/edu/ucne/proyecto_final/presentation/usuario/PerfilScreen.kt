f10b84964ea0634a5109827015ca0c77c2a03982
leodanny0216 <leodannymaria2@gmail.com>
2025-12-04 02:07:11 -0400

    PerfilScreen: Perfil del usuario

--- app/src/main/java/edu/ucne/proyecto_final/presentation/usuario/PerfilScreen.kt
+++ app/src/main/java/edu/ucne/proyecto_final/presentation/usuario/PerfilScreen.kt
@@ -1,6 +1,5 @@
 package edu.ucne.proyecto_final.presentation.usuario
 
-
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
